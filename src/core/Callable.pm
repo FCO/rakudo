e9aa878419ddf105ddf89f5854eb4461cd92bfe8
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2017-09-15 01:43:44 -0300

    First try on adding .assuming on Callable

--- src/core/Callable.pm
+++ src/core/Callable.pm
@@ -1,6 +1,10 @@
 my role Callable[::T = Mu] {
     method of() { T }
     method returns() { T }
+    method assuming(Callable:D $self: |primers) {
+        my &func = EVAL "-> {$self.signature.params>>.gist.join(", ")} \{ \$self.({$self.signature.params>>.name.join(", ")}) \}";
+        &func.assuming(|primers)
+    }
 }
 
 # vim: ft=perl6 expandtab sw=4
